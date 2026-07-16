5b8f1819239cfc3b274da79e89179dc1069d521e
Martin Eismann <softwerk@eismann.cc>
2026-07-16 17:29:51 +0200

    Increase OSR-timeout

--- app/Services/RoutingService.php
+++ app/Services/RoutingService.php
@@ -25,7 +25,7 @@ class RoutingService
 
         try {
             $response = Http::withHeaders(['Authorization' => config('services.openrouteservice.key')])
-                ->timeout(5)
+                ->timeout(15)
                 ->get("$baseUrl/v2/directions/driving-car", [
                     'start' => "{$from->longitude},{$from->latitude}",
                     'end'   => "{$to->longitude},{$to->latitude}",
