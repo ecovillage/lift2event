6768ec6b24e21f6b5f8420ba8e1a09ef9b9027bc
Martin Eismann <softwerk@eismann.cc>
2026-07-17 11:37:27 +0200

    Fix feedback email encoding

--- resources/views/emails/feedback.blade.php
+++ resources/views/emails/feedback.blade.php
@@ -1,7 +1,7 @@
-@if($name)Von: {{ $name }}
+@if($name)Von: {!! $name !!}
 @endif
-@if($email)E-Mail: {{ $email }}
+@if($email)E-Mail: {!! $email !!}
 @endif
 ---
 
-{{ $feedbackMessage }}
+{!! $feedbackMessage !!}
